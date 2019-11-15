40dc4c15911f8293263f9bdffbce4fe3c3b38fd5
timottaff <timothytaffarel@gmail.com>
2019-11-15 15:54:54 +0700

    Added config.js (For Twitter Bot)

---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    consumer_key: 'wceNDK34kBdG5y5Gb8aVlIX38',
+    consumer_secret: 'Wzj6PIN7uqablElhNjwozqj14TFQUZx4ZAn7rf9WxEF95qHfDn',
+    access_token: '4564098043-WMcUdqebUm5QRtJJQzdH713eMBvVSTEDrPzZR1K',
+    access_token_secret: '5WdZ5yplamosfu17KqZTb3AZVVmgPLnklJgQRJElvdTRs'
+
+}
